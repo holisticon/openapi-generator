b9f2e33041c405ad5df717c5f95c921cf3711503
Stefan Kuhn <stefan.kuhn@holisticon.de>
2023-03-30 12:12:46 +0200

    fix: handle uuids

--- modules/openapi-generator-azure-functions-test/src/test/kotlin/de/holisticon/CompilationTest.kt
+++ modules/openapi-generator-azure-functions-test/src/test/kotlin/de/holisticon/CompilationTest.kt
@@ -29,7 +29,7 @@ class CompilationTest : FreeSpec() {
 
   init {
     "focus" {
-      val openapiFile = openApiFiles30 / "helidon" / "petstore-for-testing.yaml"
+      val openapiFile = openApiFiles30 / "java" / "petstore-with-fake-endpoints-models-for-testing-with-http-signature-okhttp-gson.yaml"
       println("From: ${openapiFile.absPath}")
       generateOpenApi(
         openapiFile = openapiFile, to = "target" / "generated-sources",
